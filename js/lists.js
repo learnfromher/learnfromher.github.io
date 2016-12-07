1f24cfd48b1aa2717ec3cc9407d70665401408c6
meganutexas <texasmentalhealth@gmail.com>
2016-12-07 09:53:31 -0600

    abc

--- js/lists.js
+++ js/lists.js
@@ -1,7 +1,7 @@
-var phonecatApp = angular.module('listApp', []);
+var listApp = angular.module('listApp', []);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
-phonecatApp.controller('ListController', function PhoneListController($scope) {
+listApp.controller('ListController', function ListController($scope) {
   $scope.basics = [
   	{
   		name: 'Eclipse',
@@ -26,26 +26,130 @@ phonecatApp.controller('ListController', function PhoneListController($scope) {
   ];
 
   $scope.algos = [
-
+    {
+      name: 'TBD',
+      url: '#'
+    }
   ];
 
   $scope.concepts = [
+    {
+      name: 'Recursion',
+      url: '#'
+    }, 
+    {
+      name: 'Parameters',
+      url: '#'
+    },
+    {
+      name: 'Hashing',
+      url: '#'
+    },
+    {
+      name: 'Regex',
+      url: '#'
+    }
 
   ];
 
   $scope.data_structures = [
-
+    {
+      name: 'Arrays',
+      url: '#'
+    },
+    {
+      name: 'Linked Lists',
+      url: '#'
+    },
+    {
+      name: 'HashMap',
+      url: '#'
+    },
+    {
+      name: 'TreeMap',
+      url: '#'
+    },
+    {
+      name: 'Trees',
+      url: '#'
+    },
+    {
+      name: 'Binary Search Trees',
+      url: '#'
+    },
+    {
+      name: 'Tries',
+      url: '#'
+    },
+    {
+      name: 'Heaps',
+      url: '#'
+    }
   ];
 
   $scope.hacking = [
-
+    {
+      name: 'Lego Robot',
+      url: '#'
+    },
+    {
+      name: 'Ardunio',
+      url: '#'
+    },
+    {
+      name: 'Raspberry Pi',
+      url: '#'
+    },
+    {
+      name: 'Makey Makey',
+      url: '#'
+    },
+    {
+      name: 'Myo',
+      url: '#'
+    }
   ];
 
   $scope.languages = [
-
+    {
+      name: 'Java',
+      url: '#'
+    },
+    {
+      name: 'C',
+      url: '#'
+    },
+    {
+      name: 'C++',
+      url: '#'
+    },
+    {
+      name: 'Perl',
+      url: '#'
+    },
+    {
+      name: 'Python',
+      url: '#'
+    }
   ];
 
   $scope.webs = [
+    {
+      name: 'HTML',
+      url: '#'
+    },
+    {
+      name: 'CSS',
+      url: '#'
+    },
+    {
+      name: 'JavaScript',
+      url: '#'
+    },
+    {
+      name: 'Angular',
+      url: '#'
+    },
 
   ];
 
