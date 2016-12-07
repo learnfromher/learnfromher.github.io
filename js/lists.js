7186c814a9fe8cc7d4593742ca5e5e36beae55f8
Megan Avery <megan1022@utexas.edu>
2016-12-06 22:07:07 -0600

    update

---
+++ js/lists.js
@@ -0,0 +1,24 @@
+var phonecatApp = angular.module('listApp', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+phonecatApp.controller('ListController', function PhoneListController($scope) {
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+
+  $scope.basics = [
+  	{
+  		name: 'Eclipse',
+  		url: '/tutorials/basics/eclipse.html'
+  	}
+  ];
+});
